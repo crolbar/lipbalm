d4cb1663d09de58c9a13a95e5eacd1416ed8e4cb
crolbar <crolbar@crolbar.com>
2025-02-11 20:21:33 +0200

    add join tests

--- go.mod
+++ go.mod
@@ -6,4 +6,11 @@ require (
 	github.com/charmbracelet/x/ansi v0.8.0
 	github.com/mattn/go-runewidth v0.0.16
 	github.com/rivo/uniseg v0.4.7
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ join_test.go
@@ -0,0 +1,74 @@
+package lipbalm
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	// "github.com/charmbracelet/lipgloss"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+	str1 := "hello\nworld\nline\nthhreetoetuhoe\nyes"
+	str2 := "bye\nworld\nwidth\nsucks"
+
+	// fmt.Println("str1:\n", str1)
+	// fmt.Println()
+	// fmt.Println("str2:\n", str2)
+	// fmt.Println()
+
+	start := time.Now()
+	fmt.Printf("%q\n", JoinVertical(Center,
+		str1, str2,
+	))
+	fmt.Println("Without Grow:", time.Since(start))
+
+	// fmt.Println()
+
+	// fmt.Println(JoinHorizontal(Top,
+	// 	str1, str2,
+	// ))
+}
+
+func TestVJoin(t *testing.T) {
+	assert.Equal(t,
+		getDump("vertJoinExpect"),
+		JoinVertical(Left,
+			getDump("bar1"),
+			getDump("bar2"),
+		),
+	)
+}
+
+func TestHJoin(t *testing.T) {
+	assert.Equal(t,
+		getDump("horiJoinExpect"),
+		JoinHorizontal(Left,
+			getDump("bar1"),
+			getDump("bar2"),
+		),
+	)
+}
+
+func TestBothJoin(t *testing.T) {
+	assert.Equal(t,
+		getDump("go29_screen"),
+		JoinHorizontal(Top,
+			JoinVertical(Right,
+				getDump("go29_wheel"),
+				getDump("go29_bp"),
+			),
+			getDump("go29_slider"),
+		),
+	)
+}
+
+func getDump(name string) string {
+	data, err := os.ReadFile(fmt.Sprintf("tests/%s.dump", name))
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
